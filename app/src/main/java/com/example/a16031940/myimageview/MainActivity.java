1dd8cd10c91c29516f1779ff0b3d5c5404a205e7
Aiman Danish <aimanished@gmail.com>
2018-04-24 10:38:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16031940/myimageview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.a16031940.myimageview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView image2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        image2 = (ImageView)findViewById(R.id.imageView2);
+        image2.setImageResource(R.drawable.day2);
+        
+    }
+}
